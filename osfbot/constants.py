92ada64d9ef0f5f1a6ce617f93d070a97b6ea0b9
Abhigyan Trips <abhigyantrips@gmail.com>
2022-12-17 14:12:25 +0530

    feat: update constants, env

--- osfbot/constants.py
+++ osfbot/constants.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from typing import NamedTuple
 
 ENVIRONMENT = os.getenv("ENVIRONMENT")
 if ENVIRONMENT is None:
@@ -12,3 +13,19 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Paths
 EXTENSIONS = pathlib.Path("osfbot/exts/")
+
+if TEST_GUILDS := os.getenv("TEST_GUILDS"):
+    TEST_GUILDS = [int(x) for x in TEST_GUILDS.split(",")]
+
+
+class Colors:
+    blue = 0x3DB9C4
+    green = 0x53B543
+    orange = 0xF09600
+    red = 0xE72B31
+    yellow = 0xF3EA00
+
+
+class Channels(NamedTuple):
+    devlog = int(os.getenv("CHANNEL_DEVLOG", 937615771276812330))
+    log = int(os.getenv("CHANNEL_LOG", 937615771276812330))
