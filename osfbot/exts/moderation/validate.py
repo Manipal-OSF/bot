45d145f4bfde2298ca1f4d5d94944d6f89b11cc6
Abhigyan Tripathi <abhigyantrips@gmail.com>
2023-07-17 16:44:43 +0530

    feat: add validation command

--- osfbot/exts/moderation/validate.py
+++ osfbot/exts/moderation/validate.py
@@ -1,103 +1,139 @@
 from datetime import datetime
 
-from aiohttp import ClientSession
 from disnake import (
-	ApplicationCommandInteraction,
-	Embed,
-	Member,
-	Message,
-	Role,
-	TextChannel,
+    ApplicationCommandInteraction,
+    Embed,
+    Member,
+    Message,
+    Role,
+    TextChannel,
 )
 from disnake.ext.commands import Cog, slash_command
-from disnake.utils import format_dt
 from loguru import logger
 
 from ...bot import Bot
 from ...constants import Channels, Colors, Roles
 
 BASE_URL = "https://osf-database-api.shuttleapp.rs"
-DEV_BASE_URL = "http://127.0.0.1:8000"
+
 
 class OSFValidation(Cog):
-	"""Check if user is OSF member on joining."""
+    """Checks if user is OSF member on joining."""
 
-	def __init__(self, bot: Bot) -> None:
-		self.bot = bot
-		self.log_channel: TextChannel | None = None
-		self.osf_member: Role | None = None
-		super().__init__()
-	
-	async def post_message(
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+        self.log_channel: TextChannel | None = None
+        self.osf_member: Role | None = None
+        super().__init__()
+
+    async def post_message(
         self,
-        embed: Embed,
+        title: str,
+        description: str,
+        color: int = Colors.green,
     ) -> Message | None:
-		"""Send the given message in the log channel."""
-		if self.log_channel is None:
-			await self.bot.wait_until_ready()
-			self.log_channel = await self.bot.fetch_channel(Channels.log)
+        """Sends the given message in the log channel."""
+        if self.log_channel is None:
+            await self.bot.wait_until_ready()
+            self.log_channel = await self.bot.fetch_channel(Channels.log)
 
-			if self.log_channel is None:
-				logger.error(f"Failed to get log channel with ID ({Channels.log})")
-		
-		return await self.log_channel.send(embed=embed)
+            if self.log_channel is None:
+                logger.error(f"Failed to get log channel with ID ({Channels.log})")
 
-	async def process_member_status(self, member: Member) -> None:
-		"""Give the OSF Member role to validated user."""
-		if self.osf_member is None:
-			await self.bot.wait_until_ready()
-			self.osf_member = member.guild.get_role(Roles.osf_member)
+        return await self.log_channel.send(
+            Embed(
+                title=title,
+                description=description,
+                timestamp=datetime.now(),
+                color=color,
+            )
+        )
 
-			if self.osf_member is None:
-				logger.error(f"Failed to get OSF Member role with ID ({Roles.osf_member})")
-		
-		await member.add_roles(self.osf_member)
+    async def process_status(self, member: Member, status: bool | str) -> None:
+        """Gives the OSF Member role to validated user."""
+        if self.osf_member is None:
+            await self.bot.wait_until_ready()
+            self.osf_member = member.guild.get_role(Roles.osf_member)
 
-	async def validate_member_status(self, id: int) -> bool | str:
-		async with self.bot.http_session.get(
-			f"{DEV_BASE_URL}/api/v1/bot/validate", 
-			json={
-				"id": id,
-			},
-		) as resp:
-			if resp.status == 200:
-				data = await resp.json()
-				return data["result"]
-			else:
-				text = await resp.text()
-				return f"{resp.status}: {text}"
-	
-	@Cog.listener()
-	async def on_member_join(self, member: Member) -> None:
-		"""Validates user on joining the server."""
-		status = await self.validate_member_status(member.id)
+            if self.osf_member is None:
+                logger.error(
+                    f"Failed to get OSF Member role with ID ({Roles.osf_member})"
+                )
 
-		if status and isinstance(status, bool):
-			await self.process_member_status(member)
-			await self.post_message(
-				Embed(
-					title=f"OSF Member Added ({member.id})",
-					description=f"{member.mention} **({member.name}) ({member.id})** was validated by the bot.",
-					timestamp=datetime.now(),
-					color=Colors.green,
-				)
-			)
-		elif isinstance(status, str):
-			await self.post_message(
-				Embed(
-					title=f"OSF Validation Failed ({member.id})",
-					description=f"```{status}```",
-					timestamp=datetime.now(),
-					color=Colors.red,
-				)
-			)
-	
-	@slash_command()
-	async def validate(self, itr: ApplicationCommandInteraction, member: Member = None) -> None:
-		"""Command to validate users already in the server."""
-		if not member:
-			member = itr.user
+        if status and isinstance(status, bool):
+            await member.add_roles(self.osf_member)
+            await self.post_message(
+                title=f"OSF Member Added ({member.id})",
+                description=f"{member.mention} **({member.name}) ({member.id})** was validated by the bot.",
+                color=Colors.green,
+            )
+        elif isinstance(status, str):
+            await self.post_message(
+                title=f"OSF Validation Error ({member.id})",
+                description=f"```{status}```",
+                color=Colors.red,
+            )
+
+    async def validate_status(self, id: int) -> bool | str:
+        """Fetches member status from the OSF database."""
+        async with self.bot.http_session.get(
+            f"{BASE_URL}/api/v1/bot/validate",
+            json={
+                "id": id,
+            },
+        ) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                return data["result"]
+            else:
+                text = await resp.text()
+                return f"{resp.status}: {text}"
+
+    @Cog.listener()
+    async def on_member_join(self, member: Member) -> None:
+        """Validate user on joining the server."""
+        status = await self.validate_status(member.id)
+        await self.process_status(member, status)
+
+    @slash_command()
+    async def validate(
+        self, itr: ApplicationCommandInteraction, member: Member = None
+    ) -> None:
+        """Command to validate users already in the server."""
+        if not member:
+            member = itr.user
+
+        await itr.response.defer(ephemeral=True)
+
+        status = await self.validate_status(member.id)
+        await self.process_status(member, status)
+
+        if isinstance(status, bool):
+            if status is True:
+                embed = Embed(
+                    title=f"OSF Validation Successful ({member.id})",
+                    description=f"{member.mention} was verified, and has been given the relevant roles.",
+                    timestamp=datetime.now(),
+                    color=Colors.green,
+                )
+            elif status is False:
+                embed = Embed(
+                    title=f"OSF Validation Failed ({member.id})",
+                    description=f"Could not verify {member.mention} as a part of Manipal OSF.",
+                    timestamp=datetime.now(),
+                    color=Colors.orange,
+                )
+        else:
+            embed = Embed(
+                title=f"OSF Validation Error ({member.id})",
+                description="Could not validate user. Moderators have been informed of the error.",
+                timestamp=datetime.now(),
+                color=Colors.red,
+            )
+
+        await itr.edit_original_response(embed=embed)
+
 
 def setup(bot: Bot) -> None:
-	"""Loads the OSFValidation cog."""
-	bot.add_cog(OSFValidation(bot))
+    """Loads the OSFValidation cog."""
+    bot.add_cog(OSFValidation(bot))
